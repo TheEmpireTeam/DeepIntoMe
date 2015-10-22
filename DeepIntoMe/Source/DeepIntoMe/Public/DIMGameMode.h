d18aaa8edf7c01cbd0a73257c7d7a661922be9bb
Artiom Korsakov <Zver2701@gmail.com>
2015-10-22 17:01:40 +0300

    add veytilation material

---
+++ DeepIntoMe/Source/DeepIntoMe/Public/DIMGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DIMGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEEPINTOME_API ADIMGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
